501454c5659ee6d6f01fa9052e3f6e250fca9d0d
verf1CT <mishameshkoy@gmail.com>
2026-08-03 18:10:48 +0500

    chore: добавил server/.gitignore и исключить dist из git

--- server/dist/middlewares/request-id.middleware.js
+++
@@ -1,7 +0,0 @@
-import { randomUUID } from 'node:crypto';
-import { requestStore } from '../infrastructure/async-context.js';
-export function requestIdMiddleware(req, res, next) {
-    const requestId = req.headers['x-request-id'] || randomUUID();
-    res.setHeader('x-request-id', requestId);
-    requestStore.run({ requestId }, () => next());
-}
