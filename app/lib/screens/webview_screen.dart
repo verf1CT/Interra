fdedd3cf8a1ab13dcbf5a375d2be348cb48099e5
verf1CT <mishameshkoy@gmail.com>
2026-08-03 18:31:02 +0500

    fix(ci): устранить предупреждение неиспользуемого импорта и обновить документацию под Makefile и Scalar UI

--- app/lib/screens/webview_screen.dart
+++ app/lib/screens/webview_screen.dart
@@ -1,7 +1,6 @@
 import 'dart:io' show Platform;
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:url_launcher/url_launcher.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import '../config.dart';
 import '../theme.dart';
